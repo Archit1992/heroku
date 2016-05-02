0bc6008fba1aedcb094df4cda1b33b487c5b5cf2
Archit1992 <gajjararchit00004@gmail.com>
2016-05-02 11:54:14 -0400

    adding third services..

--- src/main/java/test/Sandbox.java
+++ src/main/java/test/Sandbox.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -14,10 +16,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import command.ListAllUserCommand;
 import command.ListAllUserWish;
+import command.UserInfoCommand;
 import model.User;
 import model.WishList;
 
-@Path("users")
+@Path("user")
 public class Sandbox {
 	ObjectMapper mapper = new ObjectMapper();
 
@@ -51,5 +54,23 @@ public class Sandbox {
 			}
 			return Response.status(200).entity(booksString).build();
 		}
-
-	}
+	
+	@GET
+	@Path("/{key}/{value}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUserInfo(@PathParam("key") String key, @PathParam("value") String value) throws Exception{
+			
+		UserInfoCommand userInfo = new UserInfoCommand();
+			User user = userInfo.execute(key,value);
+			String userData= null;
+			
+			try {
+				userData= mapper.writeValueAsString(user);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return Response.status(200).entity(userData).build();
+		}
+	
+	
+}
