68a546beee99d894d7f806d58c98e733ec53dce9
Archit1992 <gajjararchit00004@gmail.com>
2016-05-07 16:16:10 -0400

    adding Properties file content into MongoDB Config

--- src/main/java/mongo/ClientProvider.java
+++ src/main/java/mongo/ClientProvider.java
@@ -8,15 +8,26 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
 
+import util.PropertiesLookup;
+
 public class ClientProvider {
 	
 	MongoClient client=null;
-	
+	private static String URI = null;
 	public MongoClient getClient(){
 		// TODO Auto-generated method stub
 		try {
+			// getting a data from the Properties file...
+			PropertiesLookup lookup = new PropertiesLookup();
+			URI = "mongodb://" 
+					+lookup.getProperty("dbuser") + ":"
+					+lookup.getProperty("dbpassword") + "@ds"
+					+lookup.getProperty("dbport") + ".mlab.com:" 
+					+lookup.getProperty("dbport") + "/"
+					+lookup.getProperty("dbname");
+			System.out.println("URI is :"+URI);
 			
-			MongoClientURI uri = new MongoClientURI("mongodb://<db:user>:<db:password>@<db:port>.mlab.com:<db:port>/<db:name>");
+			MongoClientURI uri = new MongoClientURI(URI);
 			client = new MongoClient(uri); // MongoClient connected with the specified URI.
 
 			return client;
@@ -30,5 +41,13 @@ public class ClientProvider {
 	public void connectionClose(){
 		client.close();
 	}
-	
+	public static void main(String[] args) {
+		ConnectionProvider connect = new ConnectionProvider();
+		try {
+			DBCollection collectionName = connect.getCollection("imdbUser"); // for testing purposes...
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
