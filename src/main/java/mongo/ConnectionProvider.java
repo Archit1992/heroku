da10f15af03cf57df478e69368e63c18d9c82f92
Archit1992 <gajjararchit00004@gmail.com>
2016-05-07 16:14:25 -0400

    adding Properties file content into MongoDb Config.

--- src/main/java/mongo/ConnectionProvider.java
+++ src/main/java/mongo/ConnectionProvider.java
@@ -8,26 +8,38 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
 
+import util.PropertiesLookup;
+
 public class ConnectionProvider {
-	
-	MongoClient client=null;
-	
+
+	MongoClient client = null;
+	private static String URI = null;
+
 	public DBCollection getCollection(String collectionName) throws UnknownHostException {
 		// TODO Auto-generated method stub
 		try {
 			
-			MongoClientURI uri = new MongoClientURI("mongodb://<db:name>:<db:password>@ds<db:port>.mlab.com:<db:port>/<db:name>");
+			// getting a data from Properties file.
+			PropertiesLookup lookup = new PropertiesLookup();
+			URI = "mongodb://" 
+					+lookup.getProperty("dbuser") + ":"
+					+lookup.getProperty("dbpassword") + "@ds"
+					+lookup.getProperty("dbport") + ".mlab.com:" 
+					+lookup.getProperty("dbport") + "/"
+					+lookup.getProperty("dbname");
+			System.out.println("URI is :"+URI);
+			
+			MongoClientURI uri = new MongoClientURI(URI);
 			client = new MongoClient(uri); // MongoClient connected with the specified URI.
 
 			@SuppressWarnings("deprecation")
 			DB db = client.getDB(uri.getDatabase()); // Database Object created.
-			
+
 			if (db == null) {
 				System.out.println("Could not connect to Database");
 			}
-			
-			
-			DBCollection userColl = db.getCollection(collectionName);
+
+			DBCollection userColl = db.getCollection(collectionName); // send back the Collection Object.
 			return userColl;
 
 		} catch (MongoException e) {
@@ -36,8 +48,13 @@ public class ConnectionProvider {
 		return null;
 	}
 	
-	public void connectionClose(){
-		client.close();
+	public static void main(String[] args) {
+		ConnectionProvider connect = new ConnectionProvider();
+		try {
+			DBCollection collectionName = connect.getCollection("imdbUser");	// for testing purposes..
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
